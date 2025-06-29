8bc37255c2522926f3333341ae4c678fe6f21652
racero89 <monica.racero@gmail.com>
2025-06-29 13:01:31 +0200

    banderas2025

--- js/script.js
+++ js/script.js
@@ -1,23 +1,52 @@
+const countriesList = document.getElementById("countries-list");
+const info = document.getElementById("info");
 
+const getCountries = async () => {
+  try {
+    const response = await fetch("https://restcountries.com/v3/all");
+    const countries = await response.json();
+    await sortedCountries(countries);
+    return countries;
+  } catch (err) {
+    console.log("este es el error", err);
+  }
+};
 
-document.addEventListener("div",function(){
-    obtenerPaises();
-})
+const sortedCountries = (countries) => {
+  countries.sort((a, b) => {
+    const nameA = a.name.official.toUpperCase();
+    const nameB = b.name.official.toUpperCase();
+    return nameA.localeCompare(nameB);
+  });
+};
 
+getCountries().then((countries) => {
+  countries.forEach((country, index) => {
+    let template = `
+      <div class="card"> 
+      <img src=${country.flags[0]} alt=${country.name.official} />
+      <h2>${country.name.official}</h2>
+      </div>
+    `;
+    countriesList.insertAdjacentHTML("beforeend", template);
 
+    const card = document.querySelectorAll(".card")[index];
 
-function obtenerPaises() {}
-    fetch('https://restcountries.com/v3/all')
-    .then(response => response.json())
-    .then (data => {
-        const ordenarPaises = data.sort((a ,b) =>{
-            if(a < b ) return -1;
-            if(a > b) return 1;
-            return 0 ;
-         });
-         console.log(ordenarPaises);
-})
+    card.addEventListener("click", () => {
+      let templateInfo = `
+        <div class="infodetalle">
+          <img src="${country.flags[0]}" alt="${country.name.common} flag">
+            <h2>${country.name.common}</h2>
+            <p>Capital: ${country.capital}</p>
+            <p>Población: ${country.population}</p>
+            <p>Lado de la carretera: ${country.car.side}</p>
+          <button onclick="closeInfo()">cerrar</button>
+        </div>
+      `;
+      info.innerHTML = templateInfo;
+      info.classList.add("visible");
+    });
+  });
+});
 
-
-
-     
+const closeInfo = () => info.classList.remove("visible");
